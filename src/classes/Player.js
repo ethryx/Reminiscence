5afd134d00899d6ea565a08da67b29fe5571f157
Josh Schmille <josh.schmille@gmail.com>
2013-10-02 22:57:42 -0400

    started player class

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,16 +1,55 @@
-var Player = function(game, x, y, sprite, health){
+Lab.Player = function (game) {
     this.game = game;
-    this.x = x;
-    this.y = y;
-    this.sprite = sprite;
-    this.health = health || 3;
+    this.health = 100;
+    this.sprite = null;
+}
 
-    return this;
-};
+Lab.Player.prototype = {
 
-Player.prototype.create = function(x, y, s){
-    this.x = x || 100;
-    this.y = y || 100;
-    this.sprite = s || 'player';
-    return this.game.add.sprite(this.x, this.y, this.sprite);
+    add: function (x, y) {
+        this.sprite = this.game.add.sprite(x, y, 'player');
+    },
+    
+    update: function () {
+
+        this.sprite.acceleration.y = 4000;
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || this.game.input.keyboard.isDown(Phaser.Keyboard.A)){
+            this.movePlayer('LEFT');
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) || this.game.input.keyboard.isDown(Phaser.Keyboard.D)){
+            this.movePlayer('RIGHT');
+        } else {
+            if (this.sprite.velocity.x < 0){
+                this.sprite.body.velocity.x += 50;
+            } else if (this.sprite.velocity.x > 0){
+                this.sprite.body.velocity.x -= 50;
+            }
+            this.sprite.animations.stop();
+        }
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP) ||
+            this.game.input.keyboard.isDown(Phaser.Keyboard.W) ||
+            this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+            this.movePlayer('UP');
+        }
+    },
+
+    movePlayer: function (d) {
+    this.sprite.animations.play('walk', FPS, true);
+    switch(d){
+        case 'LEFT':
+            this.sprite.scale.setTo(-PLAYER_SCALE,PLAYER_SCALE);
+            this.sprite.body.velocity.x = -250;
+            break;
+        case 'RIGHT':
+            this.sprite.scale.setTo(PLAYER_SCALE,PLAYER_SCALE);
+            this.sprite.body.velocity.x = 250;
+            break;
+        case 'UP':
+            //TODO: Real ground check
+            this.sprite.animations.stop(0);
+            this.sprite.body.velocity.y = -400;
+            break;
+    }
+}
 }
--- src/game.js
+++ src/game.js
@@ -11,70 +11,32 @@ Lab.Game.prototype = {
 
 	preload: function () {
         console.log('Game started.');
-        p = new Player(this.game, this.game.world.centerX, this.game.world.centerY, 'player');
 	},
 
 	create: function () {
-        player = p.create();
+        player = new Lab.Player(this.game);
+        player.add(100, 100);
+        
         var watermark = this.game.add.sprite(0, 0, 'watermark');
         watermark.x = this.game.world.width - watermark.width;
         watermark.y = this.game.world.height - watermark.height;
 
-        player.body.bounce.y = 0.1;
-        player.body.collideWorldBounds = true;
+        player.sprite.body.bounce.y = 0.1;
+        player.sprite.body.collideWorldBounds = true;
 
-        player.anchor.setTo(0.5,0.5);
-        player.scale.setTo(PLAYER_SCALE,PLAYER_SCALE);
-        player.animations.add('walk');
+        player.sprite.anchor.setTo(0.5,0.5);
+        player.sprite.scale.setTo(PLAYER_SCALE,PLAYER_SCALE);
+        player.sprite.animations.add('walk');
 
 		this.game.input.onDown.add(this.quitToMenu, this);
 	},
 
     update: function () {
-
-        player.acceleration.y = 4000;
-
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || this.game.input.keyboard.isDown(Phaser.Keyboard.A)){
-            this.movePlayer('LEFT');
-        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) || this.game.input.keyboard.isDown(Phaser.Keyboard.D)){
-            this.movePlayer('RIGHT');
-        } else {
-            if (player.velocity.x < 0){
-                player.body.velocity.x += 50;
-            } else if (player.velocity.x > 0){
-                player.body.velocity.x -= 50;
-            }
-            player.animations.stop();
-        }
-
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP) ||
-            this.game.input.keyboard.isDown(Phaser.Keyboard.W) ||
-            this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-            this.movePlayer('UP');
-        }
+        player.update();
     },
 
 	quitToMenu: function () {
 		console.log('Quitting.');
 		this.game.state.start('mainmenu');
-	},
-
-    movePlayer: function (d) {
-        player.animations.play('walk', FPS, true);
-        switch(d){
-            case 'LEFT':
-                player.scale.setTo(-PLAYER_SCALE,PLAYER_SCALE);
-                player.body.velocity.x = -250;
-                break;
-            case 'RIGHT':
-                player.scale.setTo(PLAYER_SCALE,PLAYER_SCALE);
-                player.body.velocity.x = 250;
-                break;
-            case 'UP':
-                //TODO: Real ground check
-                player.animations.stop(0);
-                player.body.velocity.y = -400;
-                break;
-        }
-    }
+	}
 }
